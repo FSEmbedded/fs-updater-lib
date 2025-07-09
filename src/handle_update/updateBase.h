3f34b316c1100124fc27d30bcb59f8505844c838
Anatol Derksen <derksen@fs-net.de>
2025-07-09 13:18:09 +0200

    Extend updateBase with move operation deletion and improved documentation
    - Explicitly deleted move constructor and move assignment operator to
    prevent unintended moves of updateBase instances.
    - Added and improved documentation for abstract methods (install,
    rollback, getCurrentVersion).
    - Enhanced code safety and clarity for the updateBase interface.

--- src/handle_update/updateBase.h
+++ src/handle_update/updateBase.h
@@ -1,13 +1,9 @@
 #pragma once
-
 #include "updateDefinitions.h"
 #include "../uboot_interface/UBoot.h"
-
 #include "../logger/LoggerHandler.h"
 #include "../logger/LoggerEntry.h"
-
 #include "./../BaseException.h"
-
 #include <memory>
 #include <string>
 
@@ -16,42 +12,42 @@ constexpr char BASE_UPDATE[] = "base update";
 namespace updater
 {
     ///////////////////////////////////////////////////////////////////////////
-    /// firmwareUpdate declaration
+    /// updateBase declaration
     ///////////////////////////////////////////////////////////////////////////
-
     /**
      * Base class of Updater. Interface all needed functions.
      */
     class updateBase
     {
-        protected:
-            std::shared_ptr<UBoot::UBoot> uboot_handler;
-            std::shared_ptr<logger::LoggerHandler> logger;
+    protected:
+        std::shared_ptr<UBoot::UBoot> uboot_handler;
+        std::shared_ptr<logger::LoggerHandler> logger;
 
-        public:
-            updateBase(const std::shared_ptr<UBoot::UBoot> &, const std::shared_ptr<logger::LoggerHandler> &);
-            ~updateBase();
-            
-            updateBase(const updateBase &) = delete;
-            updateBase &operator=(const updateBase &) = delete;
-            updateBase(updateBase &&) = delete;
-            updateBase &operator=(updateBase &&) = delete;
+    public:
+        updateBase(const std::shared_ptr<UBoot::UBoot> &, const std::shared_ptr<logger::LoggerHandler> &);
+        virtual ~updateBase();
 
-            /**
-             * Abstract install function. Interface for install image.
-             * @param path_object
-             */
-            virtual void install(const std::string &) = 0;
+        // Disable copy and move operations
+        updateBase(const updateBase &) = delete;
+        updateBase &operator=(const updateBase &) = delete;
+        updateBase(updateBase &&) = delete;
+        updateBase &operator=(updateBase &&) = delete;
 
-            /**
-             * Abstract rollback function. Interface for roll to next older version back.
-             */
-            virtual void rollback() = 0;
+        /**
+         * Abstract install function. Interface for install image.
+         * @param path_object Path to the update image
+         */
+        virtual void install(const std::string &) = 0;
 
-            /**
-             * Return current software version.
-             * @return Software version of current state.
-             */
-            virtual version_t getCurrentVersion() = 0;
+        /**
+         * Abstract rollback function. Interface for roll to next older version back.
+         */
+        virtual void rollback() = 0;
+
+        /**
+         * Return current software version.
+         * @return Software version of current state.
+         */
+        virtual version_t getCurrentVersion() = 0;
     };
 }
